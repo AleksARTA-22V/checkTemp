b1d45f8b570e1179a73f46f90c2d1b930f79045b
AleksARTA-22V <114173054+AleksARTA-22V@users.noreply.github.com>
2022-09-22 19:25:26 +0300

    Add files via upload

---
+++ checkTemp.js Aleks-AndresRevkuts TA-22V.js
@@ -0,0 +1,12 @@
+function checkTemp (temp) {
+    if (temp >= 0 && temp <= 20) {
+
+        return -1;
+    } else if (temp >= 21 && temp <= 40) {
+        return 0;
+    } else if (temp >= 41 && temp <= 60) {
+        return 1;
+    }
+}
+
+console.log(checkTemp(45))
